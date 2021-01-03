daa1f13995662b23828b69bbbe1cc755101e9843
joseypan <joseypan@163.com>
2021-01-03 21:52:16 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+// 引入reset.css
+import './assets/styles/reset.css'
+// 引入border.css
+import './assets/styles/border.css'
+// 引入fastclick
+import fastClick from 'fastclick'
+fastClick.attach(document.body)
 
 Vue.config.productionTip = false;
 
